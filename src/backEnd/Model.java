bf41d278e68c2d1009c9273fb00d499c07935841
Francesco Agosti <fra5@duke.edu>
2013-03-04 00:34:05 -0500

    have to commit model

--- src/backEnd/Model.java
+++ src/backEnd/Model.java
@@ -55,7 +55,6 @@ public class Model {
         	output += (s + " ");
         	toExecute = function.getOutput(toExecute);
     	}
-    	System.out.println(output);
     	return output;
     	
     }
