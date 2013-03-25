32064384b7accd39cccee3df1a8bb6f3eac57478
Francesco Agosti <fra5@duke.edu>
2013-03-25 17:22:44 -0400

    added some comments

--- src/backEnd/Executable.java
+++ src/backEnd/Executable.java
@@ -1,5 +1,11 @@
 package backEnd;
 
+/**
+ * To implement this interface you must have the execute method. 
+ * Hence you are "executable". 
+ * @author Francesco Agosti
+ *
+ */
 public interface Executable {
     
     public double execute(Instruction toExecute);
