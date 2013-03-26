66891fc9260d62de0a0dfb64df6fdb77d6fae1e4
jesryu <jesryu@gmail.com>
2013-03-26 00:53:10 -0400

    required functions added for part 3

---
+++ src/functions/turtle/SetPenSize.java
@@ -0,0 +1,19 @@
+package functions.turtle;
+
+import backEnd.Model;
+import backEnd.Turtle;
+
+public class SetPenSize extends TurtleFunction {
+        
+    private static final int INPUT_VALUES = 1;
+    
+    public SetPenSize(Model model){
+        super(model, INPUT_VALUES);
+    }
+    
+    @Override
+    public void process (Turtle turtle, double[] values) {
+    	int shapeIndex = (int) values[0];
+        turtle.setShapeIndex(shapeIndex);
+    }
+}
