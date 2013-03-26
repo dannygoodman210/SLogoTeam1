3f909803bc2ad87cb89da01827cdec1adffd67f6
jesryu <jesryu@gmail.com>
2013-03-26 03:11:40 -0400

    functions working

--- src/functions/turtle/SetPenSize.java
+++ src/functions/turtle/SetPenSize.java
@@ -13,12 +13,7 @@ public class SetPenSize extends TurtleFunction {
     
     @Override
     public void process (Turtle turtle, double[] values) {
-<<<<<<< HEAD
     	int pensize = (int) values[0];
         turtle.setPenSize(pensize);
-=======
-        int penSize = (int) values[0];
-        turtle.setPenSize(penSize);
->>>>>>> 2e7df058fd05ee83987b129e6a480fd5659f57d2
     }
 }
