a3959ea4250891e352498a4485ad0bd919046bd6
ChallenHB <crnhb38@gmail.com>
2013-03-26 00:10:56 -0400

    Changing smartmap to be compatible with my version of java

--- src/backEnd/SmartMap.java
+++ src/backEnd/SmartMap.java
@@ -6,10 +6,10 @@ import java.util.Map;
 import functions.Constant;
 
 public class SmartMap {
-	private final String MAPLOADERROR = "There was an error when loading the functions!";
-	private final String MAPGETERROR = "You attempted to access an undefined variable or function";
-			
-			
+    private final String MAPLOADERROR = "There was an error when loading the functions!";
+    private final String MAPGETERROR = "You attempted to access an undefined variable or function";
+
+
 
     private Map<String, Executable> myFunctions;
     private Map<String, Executable> myUserExecutables;
@@ -17,20 +17,29 @@ public class SmartMap {
     private MapResourceManager myResourceManager;
 
     public SmartMap(Model model){
-    	myModel = model;
-    	myUserExecutables = new HashMap<String, Executable>();
+        myModel = model;
+        myUserExecutables = new HashMap<String, Executable>();
         Factory factory = new Factory();
         myResourceManager = new MapResourceManager(model);
-        
+
         try {
-			myFunctions = factory.make(model);
-		} catch (ClassNotFoundException | NoSuchMethodException
-				| SecurityException | InstantiationException
-				| IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			myModel.showErrorMsg(MAPLOADERROR);
-		}
-        
+            myFunctions = factory.make(model);
+        } catch (ClassNotFoundException e){
+            myModel.showErrorMsg(MAPLOADERROR);
+        } catch (NoSuchMethodException e){
+            myModel.showErrorMsg(MAPLOADERROR);
+        } catch (SecurityException e){
+            myModel.showErrorMsg(MAPLOADERROR);
+        } catch (InstantiationException e){
+            myModel.showErrorMsg(MAPLOADERROR);
+        } catch(IllegalAccessException e){
+            myModel.showErrorMsg(MAPLOADERROR);
+        } catch(IllegalArgumentException e){
+            myModel.showErrorMsg(MAPLOADERROR);
+        } catch(InvocationTargetException e) {
+            myModel.showErrorMsg(MAPLOADERROR);
+        }
+
     }
 
     /**
@@ -40,38 +49,38 @@ public class SmartMap {
      * @return
      */
     public Executable get(String key){
-    	int workSpaceIndex = myModel.getController().getWorkspaceIndex();
+        int workSpaceIndex = myModel.getController().getWorkspaceIndex();
         if(myFunctions.containsKey(key)){
             return myFunctions.get(key);
         }
         else if(myUserExecutables.containsKey(workSpaceIndex + key)){
-        	return myUserExecutables.get(workSpaceIndex + key);
-       	}
+            return myUserExecutables.get(workSpaceIndex + key);
+        }
         else{
-        	try{
-        		int value = Integer.parseInt(key);
-        		return new Constant(value);
-        	}catch(NumberFormatException e){
-        		myModel.showErrorMsg(MAPGETERROR);
-        	}
+            try{
+                int value = Integer.parseInt(key);
+                return new Constant(value);
+            }catch(NumberFormatException e){
+                myModel.showErrorMsg(MAPGETERROR);
+            }
         }
         return new Constant(0);
     }
-    
+
     public boolean contains(String key){
-    	return myFunctions.containsKey(key) || myUserExecutables.containsKey(key);
+        return myFunctions.containsKey(key) || myUserExecutables.containsKey(key);
     }
-    
+
     public void add(String name, Executable function) {
-    	int workSpaceIndex = myModel.getController().getWorkspaceIndex();
+        int workSpaceIndex = myModel.getController().getWorkspaceIndex();
         myUserExecutables.put(workSpaceIndex+name, function);
     }
-    
+
     public void save(){
-    	myResourceManager.saveToFile(myUserExecutables);
+        myResourceManager.saveToFile(myUserExecutables);
     }
-    
+
     public void load(String root){
-    	myResourceManager.loadFromFile(root);
+        myResourceManager.loadFromFile(root);
     }
 }
